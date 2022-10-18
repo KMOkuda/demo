d89ffdce33b3b5be3348e027ffe137a7fc528174
User105 <User105@PC110.system.local>
2022-10-18 10:09:30 +0900

    HelloResponseを試してみた

--- demo/src/main/java/com/example/demo/trySpring/HelloController.java
+++ demo/src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -11,4 +14,10 @@ public class HelloController {
 		return "hello";
 	}
 
-}
+	@PostMapping("/hello"){
+
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		model.addAttribute("sample", str);
+		return "helloResponse";
+	}
+}}
