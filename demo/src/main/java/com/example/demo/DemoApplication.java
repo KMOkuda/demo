d3cfae168495f25352733a75dd8fb887e5c13bc4
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2023-01-05 09:28:20 +0900

    ソーシャルログインサンプル

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index(@AuthenticationPrincipal OidcUser user, Model model) {
+		model.addAttribute("username", user.getFullName());
+		return "hello";
 	}
 }
